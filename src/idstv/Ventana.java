92acb7947fdbb68f3ea479afa668560f99ce4947
TavoGeraldo <147874765+TavoGeraldo@users.noreply.github.com>
2025-02-26 14:15:10 -0700

    unidad 2 ejercicio 17

--- src/idstv/Ventana.java
+++ src/idstv/Ventana.java
@@ -2,9 +2,11 @@ package idstv;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -32,14 +34,14 @@ public class Ventana extends JFrame {
 
 		this.setResizable(true);
 
-		//this.add(this.login());
-		//this.repaint();
-
-		//this.add(this.registro());
-		//this.repaint();
-		
-		this.add(this.tabla());
+		this.add(this.login());
 		this.repaint();
+
+		// this.add(this.registro());
+		// this.repaint();
+
+		// this.add(this.tabla());
+		// this.repaint();
 
 	}
 
@@ -47,56 +49,92 @@ public class Ventana extends JFrame {
 
 		JPanel panel1 = new JPanel();
 
-		panel1.setBackground(Color.decode("#e0e1dd"));
+		
 		panel1.setOpaque(true);
-		panel1.setSize(500, 500);
+		panel1.setSize(1000, 600);
 		panel1.setLocation(0, 0);
 		panel1.setLayout(null);
 
 		// Etiquetas
 		JLabel etiqueta1 = new JLabel("Nombre de usuario:");
 		etiqueta1.setSize(150, 30);
-		etiqueta1.setLocation(100, 80);
+		etiqueta1.setLocation(170, 80);
+		etiqueta1.setForeground(Color.WHITE);
 		etiqueta1.setFont(new Font("Georgia", Font.BOLD, 14));
 		panel1.add(etiqueta1);
 
 		JLabel etiqueta2 = new JLabel("Contraseña:");
 		etiqueta2.setSize(150, 30);
-		etiqueta2.setLocation(100, 160);
+		etiqueta2.setLocation(170, 160);
+		etiqueta2.setForeground(Color.WHITE);
 		etiqueta2.setFont(new Font("Georgia", Font.BOLD, 14));
 		panel1.add(etiqueta2);
 
-		
 		JLabel etiqueta4 = new JLabel("¿Olvido su contraseña?");
 		etiqueta4.setSize(150, 30);
-		etiqueta4.setLocation(220, 220);
+		etiqueta4.setLocation(290, 220);
+		etiqueta4.setForeground(Color.WHITE);
 		panel1.add(etiqueta4);
 
 		JCheckBox caja = new JCheckBox("Recordarme");
 		caja.setSize(110, 30);
-		caja.setLocation(100, 220);
-		caja.setBackground(null);
+		caja.setLocation(170, 220);
+		caja.setForeground(Color.WHITE);
+		caja.setContentAreaFilled(false);
 		panel1.add(caja);
 
 		// Campos de texto
 		JPasswordField contrasena = new JPasswordField();
 		contrasena.setSize(270, 30);
-		contrasena.setLocation(100, 190);
+		contrasena.setLocation(170, 190);
 		contrasena.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 		panel1.add(contrasena);
 
 		JTextField usuario = new JTextField();
 		usuario.setSize(270, 30);
-		usuario.setLocation(100, 110);
+		usuario.setLocation(170, 110);
 		usuario.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 		panel1.add(usuario);
 
 		// Botones
 		JButton boton = new JButton("Acceder");
-		boton.setBounds(150, 270, 170, 50);
+		boton.setBounds(220, 270, 170, 50);
 		boton.setFont(new Font("Arial", Font.BOLD, 18));
-		boton.setBorder( BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		boton.setBackground(Color.decode("#00829B"));
+		boton.setForeground(Color.WHITE);
+		boton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		panel1.add(boton);
+		
+		// Imagen usuario
+		ImageIcon icon = new ImageIcon("user.png");
+		
+		// Con esto se redimenciona la imagen para que quede bien
+		Image imagen = icon.getImage(); 
+		Image imagenNueva = imagen.getScaledInstance(30, 30, Image.SCALE_SMOOTH); 
+		ImageIcon iconoNuevo = new ImageIcon(imagenNueva);
+		//Aqui se crea la imagen ya redimencionada
+		JLabel iconoUsuario = new JLabel(iconoNuevo);
+		iconoUsuario.setSize(30, 30); 
+		iconoUsuario.setLocation(130, 110); 
+		panel1.add(iconoUsuario);
+		
+		//Imagen contraseña
+		ImageIcon icon2 = new ImageIcon("padlock.png");
+		//aqui igual se redimenciona
+		Image imagen2 = icon2.getImage(); 
+		Image imagenNueva2 = imagen2.getScaledInstance(30, 30, Image.SCALE_SMOOTH); 
+		ImageIcon iconoNuevo2 = new ImageIcon(imagenNueva2);
+		
+		JLabel iconoPassword = new JLabel(iconoNuevo2);
+		iconoPassword.setSize(30, 30); 
+		iconoPassword.setLocation(130, 190); 
+		panel1.add(iconoPassword);
+		
+		//fondo
+		JLabel fondo = new JLabel(new ImageIcon("fondo.jpg"));
+		fondo.setSize(1000, 600);
+		fondo.setLocation(0, 0);
+		panel1.add(fondo);
 
 		return panel1;
 	}
@@ -112,97 +150,93 @@ public class Ventana extends JFrame {
 
 		JLabel etiqueta1 = new JLabel("Nombre de usuario:");
 		etiqueta1.setSize(150, 30);
-		etiqueta1.setLocation(100, 40); 
+		etiqueta1.setLocation(100, 40);
 		etiqueta1.setFont(new Font("Georgia", Font.BOLD, 14));
 		panel2.add(etiqueta1);
 
-		
 		JTextField usuario = new JTextField();
 		usuario.setSize(300, 30);
 		usuario.setLocation(100, 70);
 		usuario.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 		panel2.add(usuario);
-		
+
 		JLabel etiqueta2 = new JLabel("Descripción personal:");
 		etiqueta2.setSize(170, 30);
-		etiqueta2.setLocation(100, 110); 
+		etiqueta2.setLocation(100, 110);
 		etiqueta2.setFont(new Font("Georgia", Font.BOLD, 14));
-		
+
 		panel2.add(etiqueta2);
-		
+
 		JTextArea bioArea = new JTextArea();
 		bioArea.setSize(300, 60);
 		bioArea.setLocation(100, 140);
 		bioArea.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
 
 		panel2.add(bioArea);
-		
 
 		JLabel etiqueta3 = new JLabel("Preferencias");
 		etiqueta3.setSize(150, 30);
-		etiqueta3.setLocation(190, 210); 
+		etiqueta3.setLocation(190, 210);
 		etiqueta3.setFont(new Font("Georgia", Font.BOLD, 14));
 		panel2.add(etiqueta3);
-		
+
 		JCheckBox caja1 = new JCheckBox("Dulce");
 		caja1.setSize(60, 30);
 		caja1.setLocation(100, 240);
 		caja1.setBackground(null);
 		panel2.add(caja1);
-		
+
 		JCheckBox caja2 = new JCheckBox("Salado");
 		caja2.setSize(80, 30);
 		caja2.setLocation(180, 240);
 		caja2.setBackground(null);
 		panel2.add(caja2);
-		
+
 		JCheckBox caja3 = new JCheckBox("Amargo");
 		caja3.setSize(80, 30);
 		caja3.setLocation(270, 240);
 		caja3.setBackground(null);
 		panel2.add(caja3);
-		
-		 String [] col = {"Centro", "Camino Real", "Pedregal", "Diana Laura"};
-		 JComboBox colonias = new JComboBox(col);
-		 colonias.setSize(300, 30);
-		 colonias.setFont(new Font("Georgia", Font.PLAIN, 14));
-	     colonias.setLocation(100, 280);
-		 panel2.add(colonias);
-		
+
+		String[] col = { "Centro", "Camino Real", "Pedregal", "Diana Laura" };
+		JComboBox colonias = new JComboBox(col);
+		colonias.setSize(300, 30);
+		colonias.setFont(new Font("Georgia", Font.PLAIN, 14));
+		colonias.setLocation(100, 280);
+		panel2.add(colonias);
+
 		JLabel etiqueta4 = new JLabel("Términos");
 		etiqueta4.setSize(150, 30);
-		etiqueta4.setLocation(200, 320); 
+		etiqueta4.setLocation(200, 320);
 		etiqueta4.setFont(new Font("Georgia", Font.BOLD, 14));
 		panel2.add(etiqueta4);
 
-
 		JRadioButton aceptar = new JRadioButton("Acepto los términos");
 		aceptar.setSize(150, 30);
-		aceptar.setLocation(100, 340); 
+		aceptar.setLocation(100, 340);
 		aceptar.setBackground(null);
 		panel2.add(aceptar);
-		
-	    JRadioButton noAceptar = new JRadioButton("No acepto los términos");
-	    noAceptar.setSize(160, 30);
-		noAceptar.setLocation(250, 340); 
+
+		JRadioButton noAceptar = new JRadioButton("No acepto los términos");
+		noAceptar.setSize(160, 30);
+		noAceptar.setLocation(250, 340);
 		noAceptar.setBackground(null);
 		panel2.add(noAceptar);
-		
+
 		ButtonGroup group = new ButtonGroup();
 		group.add(aceptar);
-	    group.add(noAceptar);  
-	    	    
-		
+		group.add(noAceptar);
+
 		JButton boton = new JButton("Crear");
 		boton.setSize(190, 50);
 		boton.setLocation(150, 380);
 		boton.setFont(new Font("Arial", Font.BOLD, 18));
-		boton.setBorder( BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		boton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		panel2.add(boton);
-		
+
 		return panel2;
 	}
-	
+
 	public JPanel tabla() {
 
 		JPanel panel1 = new JPanel();
@@ -212,73 +246,66 @@ public class Ventana extends JFrame {
 		panel1.setSize(1000, 600);
 		panel1.setLocation(0, 0);
 		panel1.setLayout(null);
-		
-		String[]titles= {"ID", "Nombre", "Correo electrónico", "Edad", "Estado"};
-		
-		Object [][] datos= {
-				
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				{"23454", "Juan", "Juan@gmail.mx", "18","Activo"},
-				
-				
-				
-				
-				
-				
-				
+
+		String[] titles = { "ID", "Nombre", "Correo electrónico", "Edad", "Estado" };
+
+		Object[][] datos = {
+
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+				{ "23454", "Juan", "Juan@gmail.mx", "18", "Activo" },
+
 		};
-		
-		JTable tabla = new JTable(datos,titles);
+
+		JTable tabla = new JTable(datos, titles);
 		JScrollPane scroll_pane = new JScrollPane(tabla);
 		scroll_pane.setSize(550, 200);
 		scroll_pane.setLocation(60, 220);
 		panel1.add(scroll_pane);
-		
+
 		JButton boton = new JButton("Descargar");
-		
+
 		boton.setSize(100, 50);
 		boton.setLocation(360, 150);
 		boton.setFont(new Font("Arial", Font.BOLD, 18));
-		boton.setBorder( BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		boton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		panel1.add(boton);
-		
+
 		JButton boton2 = new JButton("Añadir");
-		
+
 		boton2.setSize(100, 50);
 		boton2.setLocation(480, 150);
 		boton2.setFont(new Font("Arial", Font.BOLD, 18));
-		boton2.setBorder( BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		boton2.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		panel1.add(boton2);
-		
+
 		JLabel etiqueta1 = new JLabel("Usuarios");
 		etiqueta1.setSize(150, 30);
 		etiqueta1.setLocation(60, 80);
 		etiqueta1.setFont(new Font("Georgia", Font.BOLD, 18));
 		etiqueta1.setBackground(Color.BLUE);
 		panel1.add(etiqueta1);
-		
 
 		JLabel etiqueta2 = new JLabel("30");
 		etiqueta2.setSize(150, 30);
@@ -286,7 +313,6 @@ public class Ventana extends JFrame {
 		etiqueta2.setFont(new Font("Georgia", Font.BOLD, 18));
 		etiqueta2.setBackground(Color.BLUE);
 		panel1.add(etiqueta2);
-		
 
 		return panel1;
 	}
