e4433aed67db6cec84cc16be4820d5ac2e6121b5
TavoGeraldo <147874765+TavoGeraldo@users.noreply.github.com>
2025-03-27 14:03:35 -0700

    Funcionalidad de brocha y limpiar en Paint

--- src/idstv/Ventana.java
+++ src/idstv/Ventana.java
@@ -10,11 +10,17 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -38,8 +44,23 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
-public class Ventana extends JFrame {
+public class Ventana extends JFrame implements MouseListener, MouseMotionListener {
 
+	
+ 	
+ 	private Point lastPoint; // Para almacenar la última posición del mouse
+      // Para almacenar los puntos dibujados
+ 	private List<Point> points = new ArrayList<>();
+ 	
+    List<List<Point>> listaDePuntos = new ArrayList<>();
+	
+    private DrawingPanel drawingPanel;
+    
+    int  grosorPincel = 3;
+    int grosorMin = 1;
+    int grosorMax = 18;  
+     
+     
 	public Ventana() {
 		this.setTitle("Paint Chino");
 		this.setSize(900, 700);
@@ -60,13 +81,14 @@ public class Ventana extends JFrame {
 		this.revalidate();
 
 		this.setVisible(true);
+		
 
 	}
 	
 	public JPanel Herramientas() {
         JPanel panel1 = new JPanel();
         panel1.setBackground(Color.WHITE);
-        panel1.setLayout(new GridLayout(12, 1, 5, 5));
+        panel1.setLayout(new GridLayout(14, 1, 5, 5));
         panel1.setLocation(30, 50);
         panel1.setSize(190, 550);
         panel1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
@@ -109,6 +131,43 @@ public class Ventana extends JFrame {
         fillColor.setBackground(Color.WHITE);
         panel1.add(fillColor);
         
+        JLabel grosorLabel = new JLabel("Grosor: "+ grosorPincel);
+        panel1.add(grosorLabel);
+        
+        JPanel grosorPanel = new JPanel(new FlowLayout());
+        grosorPanel.setBackground(Color.WHITE);
+        panel1.add(grosorPanel);
+        
+        JButton menosButton = new JButton("-");
+        menosButton.setFocusPainted(false);
+        menosButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(grosorPincel > grosorMin) {
+					grosorPincel--;
+					grosorLabel.setText("Grosor: " + grosorPincel);
+				}
+				
+			}
+		});
+        grosorPanel.add(menosButton);
+        
+        JButton masButton = new JButton("+");
+        masButton.setFocusPainted(false);
+        masButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(grosorPincel < grosorMax) {
+					grosorPincel++;
+					grosorLabel.setText("Grosor: " + grosorPincel);
+				}
+				
+			}
+		});
+        grosorPanel.add(masButton);
+        
         ButtonGroup toolsGroup = new ButtonGroup();
         toolsGroup.add(brushButton);
         toolsGroup.add(eraserButton);
@@ -147,6 +206,18 @@ public class Ventana extends JFrame {
         
         JButton clearButton = new JButton("Limpiar");
         clearButton.setFocusPainted(false);
+        clearButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+		        listaDePuntos.clear();  
+		        points.clear();        
+		        drawingPanel.repaint(); 
+				
+			}
+		});
+        
+        
         panel1.add(clearButton);
         
 		
@@ -154,16 +225,115 @@ public class Ventana extends JFrame {
 	}
 	
 	public JPanel lienzo() {
+
 		JPanel panel1 = new JPanel();
+	    panel1.setBackground(Color.WHITE);
+	    panel1.setLayout(new BorderLayout()); // Asegura que el DrawingPanel se expanda
+	    panel1.setLocation(240, 50);
+	    panel1.setSize(610, 550);
+	    panel1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+	    drawingPanel = new DrawingPanel();
+	    drawingPanel.setPreferredSize(new Dimension(610, 550)); // Asegura que ocupe el espacio completo
+	    drawingPanel.addMouseListener(this); // Agregar eventos
+	    drawingPanel.addMouseMotionListener(this);
+
+	    panel1.add(drawingPanel, BorderLayout.CENTER);
+	    
+	    return panel1;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
-		panel1.setBackground(Color.WHITE);
-		panel1.setLocation(240, 50);
-		panel1.setSize(610, 550);
-		panel1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		lastPoint = e.getPoint();
+        points.add(lastPoint); // Añadir el primer punto
 		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		ArrayList<Point> listaCopiada = (ArrayList<Point>) (((ArrayList<Point>) points).clone());
+ 		
+ 		listaDePuntos.add(listaCopiada); 
+ 		points.clear();
+ 		
+ 		System.out.println(listaDePuntos);
 		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
-		return panel1;
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+ Point newPoint = e.getPoint(); 
+ 		 
+ 		 points.add(newPoint);  
+ 	        
+ 	     drawingPanel.repaint();
+ 	        
+ 	     lastPoint = newPoint;
+		
+	}
+	
+	class DrawingPanel extends JPanel {
+ 	    public DrawingPanel() {
+ 	        setBackground(Color.WHITE);
+ 	    }
+ 
+ 	    @Override
+ 	    protected void paintComponent(Graphics g) {
+ 	        super.paintComponent(g);
+ 	        Graphics2D g2d = (Graphics2D) g;
+ 	        
+ 	        // Configuración del dibujo
+ 	        g2d.setColor(Color.BLACK);
+ 	        g2d.setStroke(new BasicStroke(grosorPincel));
+ 	        
+ 	        // Dibujar todos los trazos guardados (listaDePuntos)
+ 	        for (List<Point> trazo : listaDePuntos) {
+ 	            if (trazo.size() > 1) {
+ 	                for (int i = 1; i < trazo.size(); i++) {
+ 	                    Point p1 = trazo.get(i - 1);
+ 	                    Point p2 = trazo.get(i);
+ 	                    g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+ 	                }
+ 	            }
+ 	        }
+ 	        
+ 	        // Dibujar el trazo actual (points) mientras se arrastra el mouse
+ 	        if (points.size() > 1) {
+ 	            for (int i = 1; i < points.size(); i++) {
+ 	                Point p1 = points.get(i - 1);
+ 	                Point p2 = points.get(i);
+ 	                g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+ 	            }
+ 	        }
+ 	    }
+ 	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
