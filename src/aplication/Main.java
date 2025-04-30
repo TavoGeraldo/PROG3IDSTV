9871e0aad71379294e7ec869c5d562127f18a629
TavoGeraldo <147874765+TavoGeraldo@users.noreply.github.com>
2025-04-30 14:09:31 -0700

    """""Solo para limpiar""""""
    aqui solo limpio un poco el codigo

---
+++ src/aplication/Main.java
@@ -0,0 +1,19 @@
+package aplication;
+
+import controllers.AuthController;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		AuthController app = new AuthController(); 
+ 		app.login();
+ 		System.out.println("hola");
+
+	}
+
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module InicioSesion {
-	requires java.desktop;
-}
